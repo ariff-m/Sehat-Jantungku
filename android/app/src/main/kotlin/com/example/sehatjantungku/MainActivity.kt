daa6099b6cd98b8b1d0986a27dc40c3d6208f7b1
ariff-m <21081010199@student.upnjatim.ac.id>
2024-04-29 12:07:49 +0700

    initial commit: setting up project structure

---
+++ android/app/src/main/kotlin/com/example/sehatjantungku/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sehatjantungku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:sehatjantungku/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SehatJantungku',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+         '/': (_) => const MyHomePage(),
+      },
+    );
+  }
+}
+
